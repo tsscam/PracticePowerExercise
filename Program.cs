026f6ab066717681fcb89f2e09c895c33bf16f9f
Theresa Skonieczny <tsscamaro@gmail.com>
2017-04-22 23:49:44 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("A number times itself is very POWERFUL!");
+                Console.WriteLine("###########");
+                Console.Write("Enter a Number to start : ");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the number of POWER: ");
+                int y = int.Parse(Console.ReadLine());
+                Console.WriteLine("###########");
+
+
+                long result = pow(x, y);
+
+                Console.WriteLine("The number {0}^{1} = {2} is the result", x, y, result);
+                Console.ReadKey();
+                
+            }
+        }
+            public static long pow(int x, int y)
+        {
+            long z = 1;
+            for (long i=1; i<=y; i++)
+            {
+                z = z * x;
+            }
+            return z;
+                
+        }
+    }
+}
+
+            
